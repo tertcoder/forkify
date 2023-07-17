b760c1fe9dfea4aa6ec59d7faf0a64087fd8982c
tercoder <tuyishimirebt12@gmail.com>
2023-07-17 23:20:53 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 100;
+export const RES_PER_PAGE = 10;
+export const KEY = '42d0fa52-4645-498d-9d01-40ea0b36a7d6';
+export const MODAL_CLOSE_SEC = 2.5;
